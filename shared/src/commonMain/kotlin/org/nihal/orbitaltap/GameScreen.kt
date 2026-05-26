91cc0860a95f73d1b09adb9ac00abcf401538ca9
Nihal <nihalnijuv496@gmail.com>
2026-05-26 16:04:47 +0530

    feat: added basis gamescreen with placeholders

--- shared/src/commonMain/kotlin/org/nihal/orbitaltap/GameScreen.kt
+++ shared/src/commonMain/kotlin/org/nihal/orbitaltap/GameScreen.kt
@@ -1,8 +1,91 @@
 package org.nihal.orbitaltap
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import orbitaltap.shared.generated.resources.Comfortaa_Bold
+import orbitaltap.shared.generated.resources.Comfortaa_Light
+import orbitaltap.shared.generated.resources.Comfortaa_Medium
+import orbitaltap.shared.generated.resources.Res
+import org.jetbrains.compose.resources.Font
 
 @Composable
 fun GameScreen(gameState: GameState, onGameOver: () -> Unit, onExit: () -> Unit) {
 
+    val ComfortaaFamily = FontFamily(
+        Font(Res.font.Comfortaa_Bold, FontWeight.Bold),
+        Font(Res.font.Comfortaa_Light, FontWeight.Light),
+        Font(Res.font.Comfortaa_Medium, FontWeight.Medium)
+    )
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(3, 10, 22)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text(
+            text = "OrbitalTap",
+            fontFamily = ComfortaaFamily,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Light,
+            color = Color.White
+        )
+
+        //TODO("render the map")
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+            Text(
+                text = "Score: \n${gameState.score}",
+                textAlign = TextAlign.Center,
+                fontFamily = ComfortaaFamily,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Light,
+                color = Color.White
+            )
+            //TODO("implement the timer circle")
+            Text(
+                text = "Time: \n${gameState.score}",  //temporarily showing score instead of a timer
+                textAlign = TextAlign.Center,
+                fontFamily = ComfortaaFamily,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Light,
+                color = Color.White
+            )
+        }
+
+        Spacer(Modifier.height(10.dp))
+
+        Text(
+            text = "get Question here", // placeholding the question
+            fontFamily = ComfortaaFamily,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Light,
+            color = Color.White
+        )
+
+    }
 }
